22ac5c441cde914528f8cc3b85b17a2e5f26836a
Elvio Gomes <elviobg@gmail.com>
2019-07-25 15:27:21 -0300

    Implementa o envio da resposta ao servidor

--- cripto.py
+++ cripto.py
@@ -1,7 +1,9 @@
 import requests
 import hashlib
+import json
 
 url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data'
+url_submit = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution'
 my_token = '4646090ebb3fd4037ecb08134825732b36e73e84'
 
 def decrypt(encrypted, shift_size):
@@ -14,9 +16,9 @@ def decrypt(encrypted, shift_size):
         current_ascii = ord(encrypted[i])
         
         if current_ascii >= letter_a_ascii and current_ascii <= letter_z_ascii:
-            new_ascii = current_ascii + shift_size
-            if new_ascii > letter_z_ascii:
-                new_ascii -= number_of_letters
+            new_ascii = current_ascii - shift_size
+            if new_ascii < letter_a_ascii:
+                new_ascii += number_of_letters
             decrypted += chr(new_ascii)
         else:
             decrypted += encrypted[i]
@@ -34,3 +36,9 @@ print(json_response['token'])
 print(json_response['cifrado'])
 print(json_response['decifrado'])
 print(json_response['resumo_criptografico'])
+
+file = {'answer': json.dumps(json_response, ensure_ascii=False)}
+response_post = requests.post(url_submit, params={'token': my_token}, files=file)
+print(response_post)
+print(response_post.content)
+
